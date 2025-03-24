647256485c2c2c193180586a373ed550cced6623
NechamiMargaliyot <nechami795@gmail.com>
2025-03-24 18:38:30 +0200

    add new class TestingHandleFiles

--- Thread/src/TestSaveFile/PersonForTestSaving.java
+++ Thread/src/TestSaveFile/PersonForTestSaving.java
@@ -1,4 +1,28 @@
 package TestSaveFile;
 
-public class PersonForTestSaving {
+import HandleStoreFiles.IForSaving;
+
+public class PersonForTestSaving implements IForSaving {
+    String id;
+    String name;
+
+    public PersonForTestSaving(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String getFolderName() {
+        return getClass().getPackage().getName();
+    }
+
+    @Override
+    public String getFileName() {
+        return getClass().getSimpleName()+id;
+    }
+
+    @Override
+    public String getData() {
+        return name+","+id;
+    }
 }
