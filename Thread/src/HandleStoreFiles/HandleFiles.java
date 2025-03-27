ba99d2421d945b85eee7aeadff3816723012654f
NechamiMargaliyot <nechami795@gmail.com>
2025-03-27 23:09:51 +0200

    add function readFile

--- Thread/src/HandleStoreFiles/HandleFiles.java
+++ Thread/src/HandleStoreFiles/HandleFiles.java
@@ -1,4 +1,9 @@
 package HandleStoreFiles;
+import Data.Complaint;
+import Data.Inquiry;
+import Data.Question;
+import Data.Request;
+
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,50 +12,79 @@ import java.util.Dictionary;
 import java.util.List;
 public class HandleFiles {
 
-    public void saveFile(IForSaving forSaving){
+    public void saveFile(IForSaving forSaving) {
 
         File file = new File(forSaving.getFolderName());
-        if(!file.exists())
+        if (!file.exists())
             file.mkdir();
         try {
-            BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter(file+"\\"+forSaving.getFileName()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file + "\\" + forSaving.getFileName()));
             bufferedWriter.write(forSaving.getData());
             bufferedWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    public void deleteFile(IForSaving forSaving){
-        String filePath=forSaving.getFolderName()+"\\"+forSaving.getFileName();
+
+    public void deleteFile(IForSaving forSaving) {
+        String filePath = forSaving.getFolderName() + "\\" + forSaving.getFileName();
         File file = new File(filePath);
-        if(file.exists()){
+        if (file.exists()) {
             file.delete();
-        }
-        else {
+        } else {
             System.out.println("The file don't exist.");
         }
 
     }
-    public void updateFile(IForSaving forSaving){
+
+    public void updateFile(IForSaving forSaving) {
         deleteFile(forSaving);
         saveFile(forSaving);
     }
-    private String getFileName(IForSaving forSaving)
-    {
+
+    private String getFileName(IForSaving forSaving) {
         return forSaving.getFileName();
     }
+
     private Path getDirectoryPath(IForSaving forSaving) {
-        return Paths.get(forSaving.getFolderName(),forSaving.getFileName());
+        return Paths.get(forSaving.getFolderName(), forSaving.getFileName());
     }
-    public void saveFiles(List<IForSaving> forSavingList){
-        for(IForSaving forSaving:forSavingList){
+
+    public void saveFiles(List<IForSaving> forSavingList) {
+        for (IForSaving forSaving : forSavingList) {
             saveFile(forSaving);
         }
     }
-    public void readobject(String  fileName)
-    {
-        ArrayList<String>arrayList=new ArrayList<>();
-        File file=new File(fileName);
 
+    public IForSaving readFile(File f) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        Inquiry newObj = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                for (String value : values) {
+                    arrayList.add(value);
+                }
+            }
+            switch (arrayList.get(0)) {
+                case "Question":
+                    newObj = new Question();
+                    break;
+                case "Request":
+                    newObj = new Request();
+                    break;
+                case "Complaint":
+                    newObj = new Complaint();
+                    break;
+            }
+            newObj.parseFromFile(arrayList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newObj;
     }
 }
+
+
