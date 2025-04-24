dddc87059b38bbb83a946905d0bdedff5f9f9002
nechami margaliyot <nechami795@gmail.com>
2025-04-24 21:10:21 +0300

    implementation HandaleClient

--- Thread/src/ServerSocket/HandleClient.java
+++ Thread/src/ServerSocket/HandleClient.java
@@ -1,7 +1,9 @@
 package ServerSocket;
-
+import Business.InquiryManager;
+import ClientServer.RequestData;
+import ClientServer.ResponseData;
+import ClientServer.ResponseStatus;
 import Data.Inquiry;
-import Data.Request;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
@@ -9,25 +11,41 @@ import java.util.Queue;
 
 public class HandleClient extends Thread{
     Socket clientSocket;
+    InquiryManager inquiryManager=InquiryManager.getInstance();
     public HandleClient(Socket clientSocket){
         this.clientSocket=clientSocket;
     }
     public void handleClientRequest(){
         try {
             ObjectInputStream objectInputStream =new ObjectInputStream(clientSocket.getInputStream());
-            Request obj= (Request) objectInputStream.readObject();
+            Object obj=  objectInputStream.readObject();
+            RequestData newRequest=(RequestData) obj;
             Queue<Inquiry> queue;
-            switch ()
+            ResponseData response;
+            switch (newRequest.getAction().toString())
             {
-                case "ALL_INQUIRY":queue=allInquiry();
-                break;
-                case "ADD_INQUIRY":addInquiry(obj.)
-
+                case "ALL_INQUIRY":{
+                    try {
+                        queue = inquiryManager.allInquiry();
+                        response=new ResponseData(ResponseStatus.SCCESS,"allInquiry",queue)  ;
+                    }
+                    catch (Exception e) {
+                        response=new ResponseData(ResponseStatus.FAIL,e.getMessage(),"fail") ;
+                    }
+                }
+                    break;
+                case "ADD_INQUIRY":
+                    inquiryManager.addInquiry((Inquiry) newRequest.getParameters().get(0));
+                    break;
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+    @Override
+    public void run() {
+        handleClientRequest();
     }
 }
