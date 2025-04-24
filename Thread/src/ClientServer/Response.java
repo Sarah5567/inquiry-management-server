c334bd07829be57002f5efc49748a488642a93cb
Chani Fridman <chani42665@gmail.com>
2025-04-24 16:51:28 +0300

    fill class Response

--- Thread/src/ClientServer/Response.java
+++ Thread/src/ClientServer/Response.java
@@ -1,4 +1,31 @@
 package ClientServer;
 
+import java.util.List;
+
 public class Response {
+    private InquiryManagerActions action;
+    private List<Object> parameters;
+
+    public Response(InquiryManagerActions action, List<Object>... parameters) {
+        this.action = action;
+        for(List<Object> p :parameters)
+            this.parameters.add(p);
+    }
+
+    public InquiryManagerActions getAction() {
+        return action;
+    }
+
+    public void setAction(InquiryManagerActions action) {
+        this.action = action;
+    }
+
+    public List<Object> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<Object> parameters) {
+        this.parameters = parameters;
+    }
 }
+
