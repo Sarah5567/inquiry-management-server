8f7e205382be685fc1693d54c11f470dd4dca1bf
nechami margaliyot <nechami795@gmail.com>
2025-04-24 18:18:58 +0300

    start implementation HandleClient

--- Thread/src/ServerSocket/HandleClient.java
+++ Thread/src/ServerSocket/HandleClient.java
@@ -1,4 +1,33 @@
 package ServerSocket;
 
-public class HandleClient {
+import Data.Inquiry;
+import Data.Request;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Queue;
+
+public class HandleClient extends Thread{
+    Socket clientSocket;
+    public HandleClient(Socket clientSocket){
+        this.clientSocket=clientSocket;
+    }
+    public void handleClientRequest(){
+        try {
+            ObjectInputStream objectInputStream =new ObjectInputStream(clientSocket.getInputStream());
+            Request obj= (Request) objectInputStream.readObject();
+            Queue<Inquiry> queue;
+            switch ()
+            {
+                case "ALL_INQUIRY":queue=allInquiry();
+                break;
+                case "ADD_INQUIRY":addInquiry(obj.)
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
