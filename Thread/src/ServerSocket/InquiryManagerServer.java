5976e8c0ad3f10776dfd089cb33572c72fce1f67
nechami margaliyot <nechami795@gmail.com>
2025-04-24 21:12:40 +0300

    implementation InquiryManagerServer

--- Thread/src/ServerSocket/InquiryManagerServer.java
+++ Thread/src/ServerSocket/InquiryManagerServer.java
@@ -1,4 +1,32 @@
 package ServerSocket;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class InquiryManagerServer {
-}
+    ServerSocket myServer;
+    public InquiryManagerServer(){
+        try {
+            myServer=new ServerSocket(6000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void start(){
+        try {
+            Socket socket=myServer.accept();
+            HandleClient handleClient=new HandleClient(socket);
+            handleClient.start();
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public  void stop(){
+        try {
+            myServer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+};
