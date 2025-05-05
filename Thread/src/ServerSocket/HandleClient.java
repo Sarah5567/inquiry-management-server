bfe4b4ed1c95ddaea8f014aaefddfad66d09b7ad
Chani Fridman <chani42665@gmail.com>
2025-05-05 16:15:19 +0300

    fix bug

--- Thread/src/ServerSocket/HandleClient.java
+++ Thread/src/ServerSocket/HandleClient.java
@@ -4,6 +4,7 @@ import ClientServer.*;
 import Data.Inquiry;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Queue;
 
@@ -20,7 +21,7 @@ public class HandleClient extends Thread{
             RequestData newRequest=(RequestData) objectInputStream.readObject();
             System.out.println("after recive");
             Queue<Inquiry> queue;
-            ResponseData response;
+            ResponseData response=null;
             switch (newRequest.getAction().toString())
             {
                 case "ALL_INQUIRY":{
@@ -35,8 +36,15 @@ public class HandleClient extends Thread{
                     break;
                 case "ADD_INQUIRY":
                     inquiryManager.addInquiry((Inquiry) newRequest.getParameters()[0]);
+                    // response
                     break;
+                case "TEST":
+                    response=new ResponseData(ResponseStatus.SCCESS,"test","test");
+
             }
+
+            ObjectOutputStream objectOutputStream=new ObjectOutputStream(clientSocket.getOutputStream());
+            objectOutputStream.writeObject(response);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
