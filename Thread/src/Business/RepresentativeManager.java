9a64bba074846c3d17e8f948f31e6d3f85184f9d
sarah5567 <Sarah556726@gmail.com>
2025-05-15 02:39:24 +0300

    adding queue for available representative

--- Thread/src/Business/RepresentativeManager.java
+++ Thread/src/Business/RepresentativeManager.java
@@ -1,9 +1,17 @@
 package Business;
 
+import Data.Representative;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class RepresentativeManager {
+    Queue<Representative> availableRepresentatives;
     private static RepresentativeManager instance;
 
-    private RepresentativeManager(){}
+    private RepresentativeManager(){
+        availableRepresentatives = new LinkedList<>();
+    }
     public static RepresentativeManager getInstance(){
         if (instance == null)
             instance = new RepresentativeManager();
